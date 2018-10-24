263e2b81db88fcdf1bfda440f1af5bc18af38eb0
BT_1N3_26@bktech.local <BT_1N3_26@bktech.local>
2018-10-24 12:26:14 -0400

    Initial commit

---
+++ src/HorseRunner.java
@@ -0,0 +1,55 @@
+public class HorseRunner
+{
+    public static void main(String[] args)
+    {
+        Horse horse1 = new Mustang("Secretaritat", 12000);
+        Horse horse1 = new Mustang("Dusty Trail", 22000);
+        Horse horse1 = new Mustang("Silver", 12000);
+
+        Horse[] horses = new Horse[6];
+
+        horses[1] = horse1;
+        horses[3] = horse2;
+        horses[5] = horse3;
+        HorseBarn barn = new HorseBarn(horses);
+
+        System.out.println(barn);
+        System.out.println(" Dusty Trail is in space: " + barn.findHorseSpace("Dusty Trail"));
+
+        barn.consolidate();
+
+        System.out.println(barn);
+        System.out.println(" Dusty Trail is now in space: " + barn.findHorseSpace("Dusty Trail"));
+    }
+
+    public int findHorseSpace(String name)
+    {
+        for (int i = 0; i < this.spaces.length; i++)
+        {
+            if (this.spaces[i]!=null && name.equals(this.spaces[i].getName()))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void consolidate()
+    {
+        for (int i = 0; i < this.spaces.length-1; i++)
+        {
+            if (this.spaces[i] == null)
+            {
+                for (int j = i+1; j < this.spaces.length; j++)
+                {
+                    if (this.spaces[j] != null)
+                    {
+                        this.spaces[i] = this.spaces[j];
+                        this.spaces[j] = null;
+                        j = this.spaces.length;
+                    }
+                }
+            }
+        }
+    }
+}

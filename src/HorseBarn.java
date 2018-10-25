c146c9ee327899fbef7197aedf98aed50e7279cd
clam3154 <clam3154@bths.edu>
2018-10-25 10:09:58 -0400

    So far

--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -1,10 +1,11 @@
-public class HorseRunner
+public class HorseBarn
 {
+    private Horse[] spaces;
     public static void main(String[] args)
     {
         Horse horse1 = new Mustang("Secretaritat", 12000);
-        Horse horse1 = new Mustang("Dusty Trail", 22000);
-        Horse horse1 = new Mustang("Silver", 12000);
+        Horse horse2 = new Mustang("Dusty Trail", 22000);
+        Horse horse3 = new Mustang("Silver", 12000);
 
         Horse[] horses = new Horse[6];
 
